ffc906d1e939dc9d09df414429ac9566a0348e39
Dimentiy88 <bodartinov88@gmail.com>
2023-11-25 23:10:54 +0100

    T1-L2

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Book } from 'components/Book/Book';
+import { Heading } from './components/Heading/Heading';
+
+import css from './App.module.css';
+import booksJson from './books';
+
+const books = booksJson.books;
+
+export const App = props => {
+  return (
+    <div>
+      <Heading>React lesson 2,CSS</Heading>
+      <ul className={css.booksList}>
+        {books.map((book, index) => {
+          return (
+            <Book
+              key={`${book.cover}_${book.title}`}
+              title={book.title}
+              author={book.author}
+              year={book.year}
+              genre={book.genre}
+              favourite={book.favourite}
+              cover={book.cover}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
